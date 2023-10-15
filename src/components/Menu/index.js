485a1694d439a555db05177a5975c885ad24bbac
ra1111 <rahula.raj2013@gmail.com>
2023-10-15 18:54:25 +0530

    commented others

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css';
 export const SECTIONS = {
-  PRODUCT_DESCRIPTION: 'Product Description',
-  PRODUCT_NAME: 'Product Name',
-  PRODUCT_BENEFITS: 'Product Benefits',
-  SEO_TITLE: 'SEO Title',
-  EMAIL_MARKET:'EmailMarketing',
-  ADS:"Ads",
-  WRITER_ASSITANT:'Writer Aisstant',
-  SEO_DESCRIPTION:'SEO Description',
+  // PRODUCT_DESCRIPTION: 'Product Description',
+  // PRODUCT_NAME: 'Product Name',
+  // PRODUCT_BENEFITS: 'Product Benefits',
+  // SEO_TITLE: 'SEO Title',
+  // EMAIL_MARKET:'EmailMarketing',
+  // ADS:"Ads",
+  // WRITER_ASSITANT:'Writer Aisstant',
+  // SEO_DESCRIPTION:'SEO Description',
   ARTIKLE:'SEO Article'
 };
 
